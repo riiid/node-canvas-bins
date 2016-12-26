9840854cc83d6b3bf415c26dba30cd3eef45bdca
chitacan <chitacan@gmail.com>
2016-12-26 14:25:29 +0900

    fix argument name.

--- index.js
+++ index.js
@@ -2,18 +2,16 @@ const data = require('./data/bin.json');
 const fs = require('fs');
 const path = require('path');
 
-exports.install = path => {
-  const p = path || process.cwd();
+exports.install = _path => {
+  const p = _path || process.cwd();
   data.binaries.forEach(bin => {
-    const p = path.join(p, bin.filename);
-    fs.writeFileSync(p, new Buffer(bin.content, 'base64'));
+    fs.writeFileSync(path.join(p, bin.filename), new Buffer(bin.content, 'base64'));
   });
 };
 
-exports.clean = path => {
-  const p = path || process.cwd();
+exports.clean = _path => {
+  const p = _path || process.cwd();
   data.binaries.forEach(bin => {
-    const p = path.join(p, bin.filename);
-    fs.unlinkSync(p);
+    fs.unlinkSync(path.join(p, bin.filename));
   });
 };
